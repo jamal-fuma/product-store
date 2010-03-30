1b75abf0172652c55815f63b0c0e9c92270cb4ef
David Carry <david@carry@fumasoftware.com>
2010-03-30 17:11:41 +0100

    used tools/in-place-replace.sh to change the categories to pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,17 +1,17 @@
-class Category < ActiveRecord::Base
+class Page < ActiveRecord::Base
   include ParameterScanning
   default_scope :order => 'position'
   named_scope :parents, :conditions => {:parent_id => nil}, :order => :position
   named_scope :children, :conditions => 'parent_id is not null'
-  named_scope :subcategories, lambda {|pid| {:conditions => {:parent_id => pid}, :order => :position } }
+  named_scope :subpages, lambda {|pid| {:conditions => {:parent_id => pid}, :order => :position } }
   
   # actual join tables associations
-  has_many :category_images, :dependent => :destroy, :order => :position
-  has_many :category_products, :dependent => :destroy, :order => :position
+  has_many :page_images, :dependent => :destroy, :order => :position
+  has_many :page_products, :dependent => :destroy, :order => :position
   
   # has_many associations
-  has_many :images, :through => :category_images
-  has_many :products, :through => :category_products
+  has_many :images, :through => :page_images
+  has_many :products, :through => :page_products
   
   # allow nested model creation 
   accepts_nested_attributes_for :images
@@ -39,7 +39,7 @@ class Category < ActiveRecord::Base
     validates_uniqueness_of :name, :scope => :parent_id
     def self.to_list
      list = [["Navigation",nil]]
-     Category.all.each{|c| 
+     Page.all.each{|c| 
         list << [c.long_name,c.id] 
      }
      list
@@ -48,19 +48,19 @@ class Category < ActiveRecord::Base
     def self.drop_down_list(id=nil)
       list = [["Navigation",nil]]
       if id
-          @category = Category.find(id)
-          list << [@category.long_name,@category.id]
-          list << @category.children.map{ |c|  [c.long_name,c.id] }
+          @page = Page.find(id)
+          list << [@page.long_name,@page.id]
+          list << @page.children.map{ |c|  [c.long_name,c.id] }
       else
-       Category.subcategories(id).each{|c| 
+       Page.subpages(id).each{|c| 
           list << [c.long_name,c.id] 
        }
       end
       list
     end
 
-  def categories
-       Category.subcategories(self.id) 
+  def pages
+       Page.subpages(self.id) 
   end
 
   def ancestors_name
